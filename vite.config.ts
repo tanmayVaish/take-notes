48548b23f2a083c67e2b4e11e71f751d5d242fe1
tanmayVaish <55585498+tanmayVaish@users.noreply.github.com>
2024-04-30 09:29:14 +0530

    minor changes

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://takenotes-ml.netlify.app/api',
         changeOrigin: true,
       },
     }
